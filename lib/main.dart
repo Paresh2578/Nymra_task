ba6f6c922494478f31df99ecbf1e1b0af8ae1647
Paresh Chaudhary <cparesh2005@gmail.com>
2025-06-13 22:49:32 +0530

    some chnage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo_check/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:demo_check/screens/payment_screen.dart';
 import 'package:demo_check/screens/profile_screen.dart';
@@ -21,61 +22,6 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _screens = [
-    const VideoPostScreen(),
-    const StoryUploadScreen(),
-    const ProfileScreen(),
-    const PaymentScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.video_collection_outlined),
-            selectedIcon: Icon(Icons.video_collection),
-            label: 'Feed',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.add_circle_outline),
-            selectedIcon: Icon(Icons.add_circle),
-            label: 'Upload',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.person_outline),
-            selectedIcon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.payments_outlined),
-            selectedIcon: Icon(Icons.payments),
-            label: 'Premium',
-          ),
-        ],
-      ),
     );
   }
 }
